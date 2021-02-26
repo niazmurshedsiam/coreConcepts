125a2fddeb435cdc85ccb048ac9195e428d8c159
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-26 13:17:46 +0600

    props state and api

--- src/App.js
+++ src/App.js
@@ -1,39 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  var person = {
-    name:'Karim',
-    age : 23
-  }
+ const nayoks = ['Anwar','Jafor','Alomgir','salman'];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>My Name {person.name} {person.age}</h1>
-        <h1>My Age {person.age}</h1>
-        <p>My first React Paragraph</p>
-        <Person name="Sakib al Hasan"></Person>
-        <Person name="Mustafizur"></Person>
-        <Person name="Ruble"></Person>
-        <Person name="Taskin"></Person>
-       
+        <Counter></Counter>
+        <Users></Users>
+        <Person name="Munna" job="Football"></Person>
+        <Person name="Masum" job="Cricket"></Person>
       </header>
+      
     </div>
   );
 }
-function Person(props){
-  console.log(props)
-  return (
-  <div style={{color:'yellow',background:'gray',border:'2px solid red',margin:'5px',padding:'5px'}}>
-    <h1>Name : {props.name}</h1>
-    <h3>Hero of the Year</h3>
-  </div>
+function Users(){
+  const [users,setUsers] = useState([]);
+  useEffect(() =>{
+     fetch(`https://jsonplaceholder.typicode.com/users`)
+     .then(res => res.json())
+     .then(data => setUsers(data));
+  })
+  return(
+    <div>
+      <h3>Dynamic Users : {users.length}</h3>
+      
+       <ul>
+         {
+               users.map(user => <li>{user.name} {user.email} {user.phone}</li>)
+              
+         }
+        
+       </ul>
+      
+    </div>
   )
-
 }
+function Counter(){
+  const [count,setCount] = useState(10);
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count -1);
+  return(
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={handleDecrease}>Decrease</button>
+      <button onClick={handleIncrease}>Increase</button>
+     
+    </div>
+  )
+}
+function Person(props){
+  const {name,job} = props;
+  console.log(name,job);
+  return(
+    <div>
+      <h3>My Name : {name}</h3>
+      <p>My Profession : {job}</p>
+    </div>
+  )
+}
+
 
 export default App;
